3e7fa3223d308d050395b1570ca7442fa313a64e
github username <github email address>
2019-05-07 17:39:09 -0700

    added new items and removed mp4 file

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -8,9 +8,9 @@ const DropDownMenu =(props)=>{
         <Dropdown.Menu id="dropdown_background">
           <Button.Group vertical>
             <Button onClick={props.handleFilteredItems} name='pizzaButton'>Pizza Menu</Button>
-            <Button onClick={props.handleFilteredItems} name='wingButton'>Wings Menu</Button>
+            <Button onClick={props.handleFilteredItems} name='wingButton'>Wings & Sides Menu</Button>
             <Button onClick={props.handleFilteredItems} name='beverageButton'>Beverages Menu</Button>
-            <Button ><a href="https://frontend-admin-metro-pizza.herokuapp.com/" target="_blank" rel="noopener noreferrer">To Administrator Site</a></Button>
+            <a href="https://frontend-admin-metro-pizza.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Button>Visit Administrator Site</Button></a>
           </Button.Group><hr/>
           <a href="https://github.com/YongLiang24" target="_blank" rel="noopener noreferrer">
             <Icon name="github" size='large' link/></a>
